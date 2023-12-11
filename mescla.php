83d341752255b2250ea8fd87c7c676864b6f870f
Pedro Azeredo <pedro.azeredo93@gmail.com>
2023-12-11 15:20:55 -0300

    feat: finish code

--- mescla.php
+++ mescla.php
@@ -2,42 +2,51 @@
 require('fpdf/fpdf.php');
 require('fpdi/src/autoload.php');
 
+function updateProgressBar($current, $total)
+{
+    $progress = ($current / $total) * 100;
+    $barLength = 50;
+    $numBars = round($progress / (100 / $barLength));
+    $numSpaces = $barLength - $numBars;
+
+    echo "\r[";
+    echo str_repeat("=", $numBars);
+    echo str_repeat(" ", $numSpaces);
+    echo "] " . round($progress, 2) . "%";
+}
+
 function mergePDFs($pdf1, $pdf2, $outputFilename)
 {
     $pdf = new \setasign\Fpdi\Fpdi();
 
-    // Obtenha o número de páginas
     $pagecount1 = $pdf->setSourceFile($pdf1);
     $pagecount2 = $pdf->setSourceFile($pdf2);
+    $totalPages = max($pagecount1, $pagecount2);
 
-    // Percorra cada página
-    for ($i = 1; $i <= max($pagecount1, $pagecount2); $i++) {
-        // Se ainda houver uma página no pdf1, adicione-a
+    for ($i = 1; $i <= $totalPages; $i++) {
         if ($i <= $pagecount1) {
             $pdf->setSourceFile($pdf1);
             $tplIdx = $pdf->importPage($i);
             $pdf->AddPage();
             $pdf->useTemplate($tplIdx, 0, 0, $pdf->getTemplateSize($tplIdx)['width'], $pdf->getTemplateSize($tplIdx)['height']);
-            echo "Adicionada página $i do $pdf1\n";
         }
 
-        // Se ainda houver uma página no pdf2, adicione-a
         if ($i <= $pagecount2) {
             $pdf->setSourceFile($pdf2);
             $tplIdx = $pdf->importPage($i);
             $pdf->AddPage();
             $pdf->useTemplate($tplIdx, 0, 0, $pdf->getTemplateSize($tplIdx)['width'], $pdf->getTemplateSize($tplIdx)['height']);
-            echo "Adicionada página $i do $pdf2\n";
         }
+
+        // Atualiza a barra de loading
+        updateProgressBar($i, $totalPages);
     }
 
+    echo "\nPDFs foram mesclados com sucesso!\n";
+
     // Salva o PDF resultante
-    $pdf->Output($outputFilename, 'D');
-    $pdf->Output($outputFilename, 'I');
     $pdf->Output($outputFilename, 'F');
 }
 
 // Substitua 'arquivo1.pdf', 'arquivo2.pdf' e 'output.pdf' com os nomes dos seus arquivos
-mergePDFs('arquivo1.pdf', 'arquivo2.pdf', 'output.pdf');
-
-echo 'PDFs foram mesclados com sucesso!';
+mergePDFs('arquivo1.pdf', 'arquivo2.pdf', 'output2.pdf');
